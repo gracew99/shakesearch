44e571a6736f4d18f0e6b872a649001775fba5fe
gracew99 <7.knicksfan.7@gmail.com>
2022-01-08 01:05:38 -0500

    begin add pagination

--- src/components/ResultsTable.js
+++ src/components/ResultsTable.js
@@ -7,15 +7,97 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
 import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+
+
+function TablePaginationActions(props) {
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onPageChange } = props;
+  
+    const handleFirstPageButtonClick = (event) => {
+      onPageChange(event, 0);
+    };
+  
+    const handleBackButtonClick = (event) => {
+      onPageChange(event, page - 1);
+    };
+  
+    const handleNextButtonClick = (event) => {
+      onPageChange(event, page + 1);
+    };
+  
+    const handleLastPageButtonClick = (event) => {
+      onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+  
+    return (
+      <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="first page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton
+          onClick={handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="previous page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="last page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </Box>
+    );
+  }
+  
+  TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+  };
 
 function ResultsTable(props) {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
     let navigate = useNavigate();
     async function readMore(query, index) {
         const readUrl = '/read/' + query + '/' + index;
         navigate(readUrl);
     }
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+      };
+    
+      const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+      };
 
     return (
         <div className="tableDiv">
@@ -28,20 +110,43 @@ function ResultsTable(props) {
                     <TableCell>Character Position</TableCell>
                 </TableRow>
                 </TableHead>
-                {props.results.map((result) => (
-                    <TableRow
-                    key={result.Index}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                    <TableCell align="left">{ReactHtmlParser(result.Result)}</TableCell>
-                    <TableCell component="th" scope="row">
-                        {<span className="readMore" onClick={() => readMore(props.query, result.Index)}>{result.Index}</span>}
-                    </TableCell>
-                    </TableRow>
-                ))}
                 <TableBody>
+                    {(rowsPerPage > 0
+                            ? props.results.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : props.results
+                        ).map((result) => (
+                        <TableRow
+                        key={result.Index}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell align="left">{ReactHtmlParser(result.Result)}</TableCell>
+                        <TableCell component="th" scope="row">
+                            {<span className="readMore" onClick={() => readMore(props.query, result.Index)}>{result.Index}</span>}
+                        </TableCell>
+                        </TableRow>
+                    ))}
                 
                 </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                        rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                        colSpan={2}
+                        count={props.results.length}
+                        rowsPerPage={10}
+                        page={page}
+                        SelectProps={{
+                            inputProps: {
+                            'aria-label': 'rows per page',
+                            },
+                            native: true,
+                        }}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                        ActionsComponent={TablePaginationActions}
+                        />
+                    </TableRow>
+                </TableFooter>
             </Table>
             </TableContainer>}
         </div>
